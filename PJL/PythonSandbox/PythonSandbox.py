360242dc17dd1e6b1f10b8551a348b212e77d776
paul <pauljohnleonard@gmail.com>
2017-11-18 14:37:14 +0000

    WIP

--- PJL/PythonSandbox/PythonSandbox.py
+++ PJL/PythonSandbox/PythonSandbox.py
@@ -12,12 +12,21 @@ class PythonSandbox:
         self.old_frame=None
         self.player=DiffPlayer()
 
+
+    # ###################################################################################################
+    ## Process function with without USB output
+    def process(self, inframe):
+        self._myProcess(inframe,None)
+
+
     # ###################################################################################################
     ## Process function with USB output
     def process(self, inframe, outframe):
-        # Get the next camera image (may block until it is captured) and here convert it to OpenCV BGR by default. If
-        # you need a grayscale image instead, just use getCvGRAY() instead of getCvBGR(). Also supported are getCvRGB()
-        # and getCvRGBA():
+        self._myProcess(inframe,outframe)
+
+
+    def _myProcess(self,inframe,outframe):
+
         frame = inframe.getCvBGR()
         
         w=frame.shape[1]
@@ -33,16 +42,17 @@ class PythonSandbox:
         self.timer.start()
 
 
-        if self.old_frame != None:
+        if not (self.old_frame is None):
             diff=cv2.addWeighted(frame,1.0,self.old_frame,-1.0,0)
-            outimg=frame.copy()
+            if outframe != None:
+                outimg=frame.copy()
 
             for i in range(nx):
                 i1=i*dx
                 i2=i1+dx
                 for  j in range(ny):
                     j1=j*dy
-                    j2=j1+dy
+                    j2=j1+dy 
                     sumB=0
                     sumR=0
                     sumG=0
@@ -50,21 +60,22 @@ class PythonSandbox:
                     roi=diff[j1:j2,i1:i2]
                     val=cv2.mean(roi)
                     vel = self.player.process(nn-ii-1,val)
-                    if vel > 0:
+                    if (not outframe is None) and vel > 0 :
                         cv2.rectangle(outimg,(i1,j1),(i2,j2),(255-2*vel,0,2*vel),3)
 
 
-            # Write a title:
-            cv2.putText(outimg, "PJL Python Sandbox", (3, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255),
+            if not (outframe is None):
+                # Write a title:
+                cv2.putText(outimg, "PJL Python Sandbox", (3, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255),
                         1, cv2.LINE_AA)
             
-            # Write frames/s info from our timer into the edge map (NOTE: does not account for output conversion time):
-            fps = self.timer.stop()
-            height, width, channels = outimg.shape # if outimg is grayscale, change to: height, width = outimg.shape
-            cv2.putText(outimg, fps, (3, height - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+                # Write frames/s info from our timer into the edge map (NOTE: does not account for output conversion time):
+                fps = self.timer.stop()
+                height, width, channels = outimg.shape # if outimg is grayscale, change to: height, width = outimg.shape
+                cv2.putText(outimg, fps, (3, height - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
 
-            # Convert our BGR output image to video output format and send to host over USB. If your output image is not
-            # BGR, you can use sendCvGRAY(), sendCvRGB(), or sendCvRGBA() as appropriate:
-            outframe.sendCvBGR(outimg)
+                # Convert our BGR output image to video output format and send to host over USB. If your output image is not
+                # BGR, you can use sendCvGRAY(), sendCvRGB(), or sendCvRGBA() as appropriate:
+                outframe.sendCvBGR(outimg)
 
         self.old_frame=frame
--- README_JeVois.md
+++ README_JeVois.md
@@ -2,16 +2,42 @@
 
 Make sure the web cam is not in use then
 
-OSX
+# OSX
 
 ```
 echo usbsd > /dev/cu.usbmodem1463 
 ```
 
 
-Host
 
-jevois-usbsd
+# Host:
+------
 
+
+##  Devel  
 
 jevois-deamon --serout=All
+
+
+##  Platfrom
+
+
+### Mount file system
+
+jevois-usbsd  start
+jevois-usbsd  stop
+
+
+
+
+http://jevois.org/doc/ArduinoTutorial.html
+
+JeVois allows you to store parameter settings and commands in a file named script.cfg stored in the directory of a module. The file script.cfg may contain any sequence of commands as you would type them interactively in the JeVois command-line interface.
+
+
+# Send info log messages to None, send serial strings from module to Hard serial port:
+
+setpar serlog None
+setpar serout Hard
+
+
